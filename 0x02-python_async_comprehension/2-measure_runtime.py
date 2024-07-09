335486f330ad713facbc1c9c6bcbb91ca1a279c3
Michael Gift <simiyugift@outlook.com>
2024-07-10 00:32:28 +0300

    Measure total execution time

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Contains a function to measure runtime of an async op
+"""
+import asyncio
+import time
+from importlib import import_module as using
+
+async_comprehension = using('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measures total execution time.
+    """
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time
